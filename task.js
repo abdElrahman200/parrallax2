cd229c9fe1abf31420b15bcf51abe3166e3619a0
AbdElrahman <145148937+abdElrahman200@users.noreply.github.com>
2023-10-20 17:42:54 +0300

    Add files via upload

---
+++ task.js
@@ -0,0 +1,20 @@
+const text = document.getElementById('text')
+const bird1 = document.getElementById('bird1')
+const bird2 = document.getElementById('bird2')
+const forest = document.getElementById('forest')
+const btn = document.getElementById('btn')
+const rocks = document.getElementById('rocks')
+const header = document.querySelector('.nav-bar')
+
+window.addEventListener('scroll',function(){
+ let value = window.scrollY;
+ text.style.top= 50 + value * -0.5 + '%';
+ bird1.style.top=   value * -1.5 + 'px';
+ bird1.style.left=   value * 2 + 'px';
+ bird2.style.top=   value * -1.5 + 'px';
+ bird2.style.left=   value * -5 + 'px';
+ rocks.style.marginTop=   value * 1.5 + 'px';
+ forest.style.top=   value * 0.25 + 'px';
+ header.style.top=   value * 0.5 + 'px';
+
+})
